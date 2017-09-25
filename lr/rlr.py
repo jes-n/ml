9559ec69f6201d5d9a52b3e32669337b5f36d0ec
Jesse Norris <jesse.norris95@yahoo.com>
2017-09-24 23:47:19 -0700

    Completed regularized logistic regression, but may have an error related to lambda.

--- lr/rlr.py
+++ lr/rlr.py
@@ -7,13 +7,12 @@ from scipy.optimize import minimize
 def sigmoid(z):
 	return 1/(1+np.exp(-z))
 
-def _costFunctionReg(theta, X, y):
+def _costFunctionReg(theta, X, y, lamb=0):
 	_hyp = lambda X: sigmoid(np.dot(X,theta.T))
-
 	theta = theta.reshape(1,X.shape[1])
-	return np.average(np.multiply(-y,np.log(_hyp(X))) - np.multiply((1-y),np.log(1-_hyp(X))))
+	return np.average(np.multiply(-y,np.log(_hyp(X))) - np.multiply((1-y),np.log(1-_hyp(X)))) + (lamb/(2*X.shape[1]))*np.sum(np.power(np.delete(theta, 0, axis=1),2))
 
-def costFunctionReg(theta, X, y):
+def costFunctionReg(theta, X, y, lamb=0):
 	hyp = lambda X: sigmoid(np.dot(X,theta.T))
 
 	theta = theta.reshape(1,X.shape[1])
@@ -25,6 +24,7 @@ def costFunctionReg(theta, X, y):
 
 def mapFeature(X1, X2, degree=6):
 	dim = 1
+	# print(X1.shape, X2.shape)
 	X = np.ones(X1.shape[0]).reshape(X1.shape[0], 1)
 	for i in range(1, degree+1):
 		for j in range(i+1):
@@ -44,21 +44,18 @@ X = np.column_stack((np.ones(m), X))
 X = mapFeature(X[:,[1]], X[:,[2]])
 
 theta = np.zeros((1,X.shape[1]))
-cost, grad = costFunctionReg(theta, X, y)
-print("For theta = [0, 0, 0]")
-print("Cost:", cost)
-print("Expected cost (approx): 0.693")
-print("Grad:", grad)
-print("Expected gradients (approx): [-0.1000, -12.0092, -11.2628]")
+# cost, grad = costFunctionReg(theta, X, y)
+# print("For theta = [0, 0, 0]")
+# print("Cost:", cost)
+# print("Expected cost (approx): 0.693")
+# print("Grad:", grad)
+# print("Expected gradients (approx): [-0.1000, -12.0092, -11.2628]")
 
-# res = minimize(_costFunctionReg, theta, args=(X, y),
-# 	method='BFGS', options={"maxiter":400}, callback=None)
+res = minimize(_costFunctionReg, theta, args=(X, y),
+	method='BFGS', options={"maxiter":400}, callback=None)
 
-# print("\nSolution:", res.x, "with cost =", _costFunctionReg(res.x.reshape(1,28), X, y))
-# # print("Expected cost (approx):", 0.203)
-# # print("Prediciton:", float(sigmoid(np.dot(np.array([1, 45, 85]), res.x.reshape(1,28).T))))
-# # print("Expected prediction of", 0.776)
-# theta = res.x
+print("\nSolution:", res.x, "with cost =", _costFunctionReg(res.x.reshape(1,28), X, y))
+theta = res.x
 
 fig = plt.figure(figsize=(7,6))
 ax = fig.add_subplot(111)
@@ -74,12 +71,26 @@ for i, test in enumerate(y):
 	else:
 		a = ax.scatter(X[i,1], X[i,2], color="#003262", marker="+", s=75, alpha=0.75, edgecolor='k')
 
-# boundary = lambda x, theta: (-1.0/theta[2])*(theta[1]*x + theta[0])
-# x = np.linspace(29, 101, 50)
-# plt.plot(x, boundary(x, theta), color="#3B7EA1", alpha=0.75, lw=0.85, ls="--")
+delta = 0.05
+u = np.arange(-0.8, 1.2, delta)
+v = np.arange(-0.8, 1.2, delta)
+U, V = np.meshgrid(u, v)
+W = np.zeros((len(u),len(v)))
+for i in range(len(u)):
+	for j in range(len(v)):
+		# print(mapFeature(u([]), v(j)))
+		# print(u[i].reshape(1,1).shape, v[j].shape
+		# print(mapFeature(u[i].reshape(1,1), v[j].reshape(1,1)))
+		# print(np.dot(mapFeature(u[i].reshape(,11), v[j].reshape(1,1)), theta.T))
+		W[i,j] = np.dot(mapFeature(u[i].reshape(1,1), v[j].reshape(1,1)), theta.T)
+
+print(W)
+plt.contour(U, V, W.T, levels=[0], cmap=plt.cm.Paired, linestyles="dashed")
+# plt.colorbar()
 
 plt.xlim(-0.95, 1.2)
 plt.ylim(-0.8, 1.2)
+plt.title(r"$\lambda=0$")
 plt.xlabel("Microchip Test 1")
 plt.ylabel("Microchip Test 2")
 plt.legend([na, a], ["y=0", "y=1"], loc="upper right", scatterpoints=1, fancybox=False)
