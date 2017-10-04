106a33bbb69e1bb22835318e1c89d68830803ca5
Jesse Norris <jesse.norris95@yahoo.com>
2017-10-03 23:36:37 -0700

    Regularization implemented in 'nn/bnn.py', also included 3 test cases.

--- nn/bnn.py
+++ nn/bnn.py
@@ -8,6 +8,9 @@ from displayex3 import displayData, displayImage
 def sigmoid(z):
 	return 1/(1+np.exp(-z))
 
+def sigprime(z):
+	return sigmoid(z)*(1-sigmoid(z))
+
 def costFunction(Theta, ninput, nhidden, nclasses, X, y, lamb=0, check=False):
 	[m,n] = X.shape
 	### Add the bias array to 'X'
@@ -44,27 +47,10 @@ def costFunction(Theta, ninput, nhidden, nclasses, X, y, lamb=0, check=False):
 		cost[i] += np.sum(np.multiply(-cat,np.log(a3[[i]])) - np.multiply((1-cat),np.log(1-a3[[i]])), axis=1)
 		# print("cost[{}] = {}".format(i, cost[i]))
 
-	return np.average(cost, axis=0)
+	### Apply Regularization
+	reg = (lamb/2/m)*(np.sum(np.sum(np.power(Theta1[:,1:],2), axis=1), axis=0) + np.sum(np.sum(np.power(Theta2[:,1:],2), axis=1), axis=0))
 
-##### Test case using weight vectors from 'ex3'
-# Theta1 = np.loadtxt("ex3weights1.csv", delimiter=",")
-# Theta2 = np.loadtxt("ex3weights2.csv", delimiter=",")
-# Theta = np.concatenate((Theta1.ravel(), Theta2.ravel()))
-# cost = costFunction(Theta, 400, 25, 10, X, y)
-# print("Cost:", cost, "(should be 0.287629)")
-##############
-
-##### Test case for 'ex4'
-##### https://www.coursera.org/learn/machine-learning/discussions/weeks/5/threads/uPd5FJqnEeWWpRIGHRsuuw
-# il = 2
-# hl = 2
-# nl = 4
-# nn = (np.arange(1,18+1)/10).reshape(18,1)
-# X = np.cos(np.array([[1, 2], [3, 4], [5, 6]]))
-# y = np.array([[4], [2], [3]])
-# cost = costFunction(nn, il, hl, nl, X, y)
-# print("Cost:", cost, "(should be 7.4070)")
-##############
+	return np.average(cost, axis=0) + reg
 
 ### "ex3data1.csv" contains 5,000 samples of 401 element arrays
 ### These correspond to 20x20 pixel images (400 elements) and 1 classification
@@ -90,25 +76,37 @@ Theta1 = np.subtract(np.multiply(2*epsilon, np.random.rand(s2,s1+1)), epsilon)
 [m1,n1] = Theta1.shape
 Theta2 = np.subtract(np.multiply(2*epsilon, np.random.rand(s3,s2+1)), epsilon)
 [m2,n2] = Theta2.shape
-
 ## Flatten into a single theta matrix
+Theta = np.concatenate((Theta1.ravel(), Theta2.ravel()))
 
-## Verify the dimensions and type of 'X', 'y', and the weight matricies
-print("X:", X.shape, type(X))
-print("y:", y.shape, type(y))
-print("Theta1:", Theta1.shape, type(Theta1))
-print("Theta2:", Theta2.shape, type(Theta2))
-print("Theta:", Theta.shape, type(Theta))
+##### Test case using weight vectors from 'ex3'
+# Theta1 = np.loadtxt("ex3weights1.csv", delimiter=",")
+# Theta2 = np.loadtxt("ex3weights2.csv", delimiter=",")
+# Theta = np.concatenate((Theta1.ravel(), Theta2.ravel()))
+# cost = costFunction(Theta, s1, s2, s3, X, y)
+# print("Cost:", cost, "(should be 0.287629)")
+##############
 
-# Loop over the first
-cost = 0
-for i in range(k):
-	cat = [1 if (int(val)%10 == i) else 0 for val in y]
-	cat = np.array((cat)).reshape(m,1)
-	cost += costFunction(Theta1, X, cat, check=True)
-	a2 = sigmoid(np.dot(X,Theta1.T))
-	a2 = np.insert(a2, 0, np.ones((1,m)), axis=1)
-	cost += costFunction(Theta2, a2, cat, check=True)
+##### Test case for 'ex4'
+##### https://www.coursera.org/learn/machine-learning/discussions/weeks/5/threads/uPd5FJqnEeWWpRIGHRsuuw
+# il = 2
+# hl = 2
+# nl = 4
+# nn = (np.arange(1,18+1)/10).reshape(18,1)
+# X = np.cos(np.array([[1, 2], [3, 4], [5, 6]]))
+# y = np.array([[4], [2], [3]])
+# cost = costFunction(nn, il, hl, nl, X, y)
+# print("Cost:", cost, "(should be 7.4070)")
+# cost = costFunction(nn, il, hl, nl, X, y, lamb=4, check=True)
+# print("Cost:", cost, "(should be 19.474)")
+##############
 
-print(cost)
+### Verify the dimensions and type of 'X', 'y', and the weight matricies
+# print("X:", X.shape, type(X))
+# print("y:", y.shape, type(y))
+# print("Theta1:", Theta1.shape, type(Theta1))
+# print("Theta2:", Theta2.shape, type(Theta2))
+# print("Theta:", Theta.shape, type(Theta))
 
+cost = costFunction(Theta, s1, s2, s3, X, y, lamb=1, check=True)
+print("Cost:", cost, "(should be 0.383770)")
