e71da12a33238f4539dc13eb72344b36d8f4bd5c
Jesse Norris <jesse.norris95@yahoo.com>
2017-10-07 03:08:03 -0700

    Update towards back propagation.

--- nn/bnn.py
+++ nn/bnn.py
@@ -22,14 +22,14 @@ def costFunction(Theta, ninput, nhidden, nclasses, X, y, lamb=0, check=False):
 		print("Warning: 'X' is a square matrix, ensure it is of dimensions (# features)x(# data points)")
 	### Add the bias array to 'X'
 	X = np.concatenate((np.ones((1,m)), X))
-	
-	### Unflatten 'Theta' into 'Theta1' and 'Theta2'
-	Theta1 = Theta[:(nhidden*(ninput+1))].reshape((ninput+1), nhidden)
-	Theta2 = Theta[(nhidden*(ninput+1)):].reshape((nhidden+1), nclasses)
-
 	if (check):
 		print("X:", X.shape, type(X))
 		print("y:", y.shape, type(y))
+
+	### Unflatten 'Theta' into 'Theta1' and 'Theta2'
+	Theta1 = Theta[:(nhidden*(ninput+1))].reshape((ninput+1), nhidden)
+	Theta2 = Theta[(nhidden*(ninput+1)):].reshape((nhidden+1), nclasses)
+	if (check):
 		print("Theta:", Theta.shape, type(Theta))
 		print("Theta1:", Theta1.shape, type(Theta1))
 		print("Theta2:", Theta2.shape, type(Theta2))
@@ -39,12 +39,9 @@ def costFunction(Theta, ninput, nhidden, nclasses, X, y, lamb=0, check=False):
 	a2 = np.concatenate((np.ones((1,m)), a2)) # Add the bias vector to 'a2'
 	### Determine the third activiation function (the hypothesis)
 	a3 = sigmoid(np.dot(Theta2.T,a2))
-	
-	### Check the dimensionality
 	if (check):
 		print("a2:", a2.shape, type(a2))
 		print("a3:", a3.shape, type(a3))
-	###############
 
 	### Vectorize the classification for each data point
 	cat = np.zeros((nclasses,m))
@@ -52,8 +49,6 @@ def costFunction(Theta, ninput, nhidden, nclasses, X, y, lamb=0, check=False):
 		yi = int(y[i])
 		cat[:,i] = [1 if (val == yi) else 0 for val in range(1,nclasses+1)]
 	y = cat
-	if (check):
-		print("y:", y.shape, type(y))
 
 	### Forward Propagate to determine the cost
 	cost = np.sum(np.multiply(-y,np.log(a3)) - np.multiply((1-y),np.log(1-a3)), axis=0)
@@ -61,10 +56,11 @@ def costFunction(Theta, ninput, nhidden, nclasses, X, y, lamb=0, check=False):
 		print("Cost:", cost.shape, type(cost))
 
 	### Back Propagate to determine the deltas
-	# print(d3.shape, d3[[i]].shape)
-	# d3[[i]] = a3[[i]] - cat
-	# print(Theta2.T.shape, d3[[i]].T.shape, a2.T.shape, Theta2.shape)
-	# d2[[i]] = np.multiply(np.multiply(Theta2.T, d3[[i]].T), sigprime(np.dot(a2[[i]],Theta2.T)))
+	# d3 = a3 - y
+	# d2 = np.multiply(np.multiply(Theta2.T,d3), sigprime(np.dot(Theta2.T,a2)))
+	# if (check):
+	# 	print("d3:", d3.shape, type(d3))
+	# 	print("d2:", d2.shape, type(d2))
 
 	### Apply Regularization
 	regularization = (lamb/(2*m))*(np.sum(np.power(Theta1[1:,:],2)) + np.sum(np.power(Theta2[1:,:],2)))
@@ -124,32 +120,30 @@ Theta = np.concatenate((Theta1.ravel(), Theta2.ravel()))
 
 ##### Test case for 'ex4'
 ##### https://www.coursera.org/learn/machine-learning/discussions/weeks/5/threads/uPd5FJqnEeWWpRIGHRsuuw
-# il = 2
-# hl = 2
-# nl = 4
-# nn = np.arange(1,18+1)/10
-# X = np.cos(np.array([[1, 2], [3, 4], [5, 6]]))
-# y = np.array([[4], [2], [3]])
-# print("Unregularized 'ex4' test (from forum)")
-# cost = costFunction(nn, il, hl, nl, X, y, check=True)
-# print("Cost:", cost, "(should be 7.4070)\n")
-# print("Regularized 'ex4' test (from forum)")
-# cost = costFunction(nn, il, hl, nl, X, y, lamb=4, check=True)
-# print("Cost:", cost, "(should be 19.474)\n")
+il = 2
+hl = 2
+nl = 4
+nn = np.arange(1,18+1)/10
+X = np.cos(np.array([[1, 2], [3, 4], [5, 6]]))
+y = np.array([[4], [2], [3]])
+print("Unregularized 'ex4' test (from forum)")
+cost = costFunction(nn, il, hl, nl, X, y, check=True)
+print("Cost:", cost, "(should be 7.4070)\n")
+print("Regularized 'ex4' test (from forum)")
+cost = costFunction(nn, il, hl, nl, X, y, lamb=4, check=True)
+print("Cost:", cost, "(should be 19.474)\n")
 ##############
 
-# ### Verify the dimensions and type of 'X', 'y', and the weight matricies
-# print("X:", X.shape, type(X))
-# print("y:", y.shape, type(y))
-# print("Theta1:", Theta1.shape, type(Theta1))
-# print("Theta2:", Theta2.shape, type(Theta2))
-# print("Theta:", Theta.shape, type(Theta))
+def callback(xk):
+	print(costFunction(xk))
 
 ### Determine the weight vector for each classification
 ###################
 ### This will be done by iterating over each class
 ### A Conjugate Gradient minimization will be preformed to determine each set of weight vectors
 ###################
-start = time.time() # For minimization timing purposes
-cost = costFunction(Theta, s1, s2, s3, X, y, check=True)
-print("Time elapsed:", time.time() - start, "seconds") # Show the elapsed time
+# start = time.time() # For minimization timing purposes
+# # cost = costFunction(Theta, s1, s2, s3, X, y, check=True)
+# res = minimize(costFunction, Theta, args=(s1, s2, s3, X, y),
+# 	method='CG', options={"disp":True, "gtol":5e-4}, callback=callback)
+# print("Time elapsed:", time.time() - start, "seconds") # Show the elapsed time
